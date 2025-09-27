1d32e3244256e4ed27857cbaa2c4cf55b833add0
Ethan <90428291+asiankidtv@users.noreply.github.com>
2025-09-26 22:53:34 -0400

    Main File

---
+++ main.py
@@ -0,0 +1,20 @@
+from zipfile import ZipFile
+import os
+
+# Modify Base on current working folder:
+CURRENT_DIRECTORY = "chartFolders/PRISM PLUS"
+RESULT_DIRECTORY = "chartZips/PRISM PLUS"
+
+chartPaths = {}
+
+for root, directories, files in os.walk(CURRENT_DIRECTORY):
+    if root != CURRENT_DIRECTORY:
+        chartPaths[root] = files
+
+for chartPath in chartPaths:
+    chartName = chartPath.split(f"{CURRENT_DIRECTORY}\\")[1]
+    zipName = f"{RESULT_DIRECTORY}/{chartName}.zip"
+    
+    with ZipFile(zipName, "w") as zip:    
+        for file in chartPaths[chartPath]:
+            zip.write(f"{chartPath}/{file}", file)
